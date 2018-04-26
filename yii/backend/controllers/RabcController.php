2dce8a70424b1c343b70f5736d93176102d1e2ab
xzhaha123 <595075435@qq.com>
2018-04-26 17:38:51 +0800

    4.26

--- yii/backend/controllers/RabcController.php
+++ yii/backend/controllers/RabcController.php
@@ -45,16 +45,28 @@ class RabcController extends Controller
     public function actionUsers()
     {
         $auth=Yii::$app->authManager;
-        //创建权限
-        $a = $auth->createPermission('测试权限');
-        $a->description = '测试权限1';
-        $uid = Yii::$app->getRequest()->get('uid');
-        $allPermission = '';
+//        //创建权限
+//        $a = $auth->createPermission('测试权限');
+//        $a->description = '测试权限1';
+//        $uid = Yii::$app->getRequest()->get('uid');
+//        $allPermission = '';
+//
+//        //创建角色
+//        $name = 'admin';
+//        $role = $auth->createRole($name);
+//        $role->description = '创建了 ' . $name. ' 角色';
+//        $auth->add($role);
+//
+//        //将权限分给角色
+//        $items = ['role'=>'admin','permission'=>'测试权限'];
+//        $parent = $auth->createRole($items['role']);                //创建角色对象
+//        $child = $auth->createPermission($items['permission']);     //创建权限对象
+//        $auth->addChild($parent, $child);                           //添加对应关系
 
-        //创建角色
-        $name = '系统管理员';
-        $role = $auth->createRole($name);
-        $role->description = '创建了 ' . $name. ' 角色';
-        $auth->add($role);
+        //将角色附给用户
+        $items = ['role'=>'admin'];
+        $role = $auth->createRole($items['role']);                //创建角色对象
+        $user_id = 11;                                             //获取用户id，此处假设用户id=1
+        $auth->assign($role, $user_id);                           //添加对应关系
     }
 }
